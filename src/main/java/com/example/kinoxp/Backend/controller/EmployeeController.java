345bc6eeb063e611e3f88de4473d59aaa65c2e8a
najamoe <najamoe@outlook.dk>
2023-10-03 21:43:35 +0200

    localhost:2020/login working -NEED TO FIX IMG FILE

--- src/main/java/com/example/kinoxp/Backend/controller/EmployeeController.java
+++ src/main/java/com/example/kinoxp/Backend/controller/EmployeeController.java
@@ -1,7 +1,15 @@
 package com.example.kinoxp.Backend.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EmployeeController {
 
+    @GetMapping("/login")
+    public String showLogin(){
+        return "login";
+    }
 //Login method for employees
 
 
